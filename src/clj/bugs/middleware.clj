86131d686704318892a4124df55870ee4c198848
eightytwo <eightytwo@users.noreply.github.com>
2020-12-03 15:04:44 +1000

    Redirect calls to /api to the API subdomain

--- src/clj/bugs/middleware.clj
+++ src/clj/bugs/middleware.clj
@@ -1,15 +1,31 @@
 (ns bugs.middleware
-  (:require [next.jdbc :as jdbc]
-            [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [next.jdbc :as jdbc]
+            [ring.util.request :as request]
+            [ring.util.response :as response]))
 
 (def api-subdomain-to-path
   {:name    ::api-subdomain-to-path
    :compile (fn [_ _]
               (fn [handler _]
                 (fn [req]
+                  ;; If the call is to api.example.com then append the /api directory.
+                  ;; This is needed because all API routes fall under /api.
                   (if (str/starts-with? (:server-name req) "api.")
                     (handler (assoc req :uri (str "/api" (:uri req))))
-                    (handler req)))))})
+
+                    ;; If the call is to example.com/api then redirect to api.example.com,
+                    ;; otherwise let it pass through.
+                    (if (str/starts-with? (:uri req) "/api")
+                      (let [host (get-in req [:headers "host"])
+                            new-url (str/replace-first (request/request-url req)
+                                                       (str host "/api")
+                                                       (str "api." host))
+                            code (if (= (:request-method req) :get)
+                                   :moved-permanently
+                                   :permanent-redirect)]
+                        (response/redirect new-url code))
+                      (handler req))))))})
 
 (def db
   {:name    ::db
